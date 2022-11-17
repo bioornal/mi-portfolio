3f811e05b6bf4b1099e6ceca4b73b9f59365f300
ChristianSpeziali <spezialichristian@gmail.com>
2022-11-17 13:10:21 -0300

    probando sin el header2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/Header'
+// import Header from './components/Header/Header'
 import About from './components/About/About'
 import Projects from './components/Projects/Projects'
 import Skills from './components/Skills/Skills'
