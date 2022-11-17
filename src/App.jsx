5c8a8c442a36f8de1b16c50acb7980d9fa50cb2b
ChristianSpeziali <spezialichristian@gmail.com>
2022-11-17 13:23:26 -0300

    probando otro4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 
 import './App.css'
-import Header from './components/Header/Header'
-import Projects from './components/Projects/Projects'
-import About from './components/About/About'
-import Skills from './components/Skills/Skills'
-import Contact from './components/Contact/Contact'
-import ScrollToTop from './components/ScrollToTop/ScrollToTop'
-import Footer from './components/Footer/Footer'
+// import Header from './components/Header/Header'
+// import Projects from './components/Projects/Projects'
+// import About from './components/About/About'
+// import Skills from './components/Skills/Skills'
+// import Contact from './components/Contact/Contact'
+// import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+// import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
